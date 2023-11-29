72363d2daa1ab1f7fbe94293a019dce3f693c2bd
Ziad Hesham <ziad.hesham@ejust.edu.eg>
2023-11-29 20:53:28 +0200

    pgtbl

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,20 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 
+sys_trace(void)
+{
+  if (argint(0, &myproc()->tracemask) < 0){return -1;}
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  uint64 info;
+
+  if(argaddr(0, &info) < 0)
+    return -1;
+  return systeminfo(info);
+}
