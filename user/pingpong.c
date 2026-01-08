78dfd0d026e5f32a5eb24bb9e367e1f257614377
ziad-rohayiem <ziad.hesham@ejust.edu.eg>
2026-01-08 20:40:23 +0200

    pingpong solution

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,31 +1,28 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
+// #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[])
-{
-	int p1[2];
-	pipe(p1);
-	char buf[20];
-	int pid = fork();
+// There are two main concerns I discovered with my solution here. The first is that I am using one pipe for both parent and child
+// if not handled properly. better practice is to use p1 (parent -> child) so parent only writes for p1 and p2 (child -> parent)
+// and carefully close each fd once it is not used. The second comment is that I am not really handling any errors from pipe,
+// fork, read, or write. These errors have to be handled carefully for correct programs in a real OS.
 
-	if(pid==0){
-		read(p1[0], buf, 1);
-		close(p1[0]);
-		printf("%d: received ping\n", getpid());
-		write(p1[1], "@", 1);
-		close(p1[1]);
-		exit(0);
-	}
-	
-	else if(pid>0){
-		write(p1[1], "@", 1);
-		close(p1[1]);
-		read(p1[0], buf, 1);
-		close(p1[0]);
-		printf("%d: received pong\n", getpid());
-		exit(0);
-	}
-	
-	return 0;
+int main(int argc, char *argv[]) {
+  if (argc != 1) {
+    fprintf(2, "Usage: pingpong\n");
+    exit(1);
+  }
+  int p[2];
+  pipe(p);
+  char buf[1];
+  if (fork() == 0) {            // child
+    read(p[0], buf, 1); // read byte
+    printf("%d: received ping\n", getpid());
+    write(p[1], buf, 1);                 // write byte second time
+  } else {                               // parent
+    write(p[1], "b" /*random byte*/, 1); // write byte first time
+    read(p[0], buf, 1);          // read byte second time
+    printf("%d: received pong\n", getpid());
+  }
+  exit(0);
 }
