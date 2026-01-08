4f2945d91c63fa5a3f5b10028d29a051c1ba2f13
ziad-rohayiem <ziad.hesham@ejust.edu.eg>
2026-01-08 20:40:37 +0200

    primes solution

--- user/primes.c
+++ user/primes.c
@@ -1,57 +1,52 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
+// #include "kernel/stat.h"
 #include "user/user.h"
 
-void rec(int fd)
-{
-    int num;
-    read(fd, &num, sizeof(int));
-    printf("prime %d\n", num);
-    
-    int p[2];
-    pipe(p);
-    int tmp = -1;
-    while (1) {
-        int n = read(fd, &tmp, sizeof(int));
-        if (n <= 0) {
-            break;
-        }
-        if (tmp % num != 0) {
-            write(p[1], &tmp, sizeof(int));
-        }
-    }
-    if (tmp == -1) {
-        close(p[1]);
-        close(p[0]);
-        close(fd);
-        return;
-    }
-    int pid = fork();
-    if (pid == 0){
-        close(p[1]);
-        close(fd);
-        rec(p[0]);
-        close(p[0]);
-    }
-    else {
-        close(p[1]);
-        close(p[0]);
-        close(fd);
-        wait(0);
-    }
+int recurse_prime(void);
+
+int main(int argc, char *argv[]) {
+  if (argc != 1) {
+    fprintf(2, "Usage: primes\n");
+    exit(1);
+  }
+  int next_pipe_write = recurse_prime();
+
+  for (int i = 2; i < 36; i++) {
+    write(next_pipe_write, &i, 4);
+  }
+  close(next_pipe_write);
+  wait(0);
+  exit(0);
 }
 
-int main(int *argc, char **argv[])
-{
-    int p[2];
-    pipe(p);
-    for (int i = 2; i < 35; i++) {
-        //int n = i;
-        write(p[1], &i, sizeof(int));
-    }
+int recurse_prime(void) {
+  int p[2];
+  pipe(p);
+  if (fork() == 0) {
     close(p[1]);
-    rec(p[0]);
+    int prime;
+    read(p[0], &prime, 4);
+    printf("prime %d\n", prime);
+    int others;
+    int next_pipe_write = -1;
+    while (read(p[0], &others, 4) != 0) {
+      if (others % prime != 0) {
+        if (next_pipe_write == -1) {
+          next_pipe_write = recurse_prime();
+        }
+        write(next_pipe_write, &others, 4);
+      }
+    }
+    if (next_pipe_write != -1)
+      close(next_pipe_write);
+    wait(0); // if you remove this line it prints correctly except that the $
+    // will be misaligned (still working try echo). this is because the $ prints
+    // after the top-level parent exits. note that it only waits its child. if
+    // you don't make each proc wait its child you will end up with children
+    // printing primes after the top-level paren exits. this is problematic.
+    exit(0);
+  } else {
     close(p[0]);
-
-    exit(1);
+    return p[1];
+  }
 }
